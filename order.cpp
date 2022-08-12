2282a31c46864e8aaecc654d0a843e0121adfbfc
TheFormat <95959211+TheFormat@users.noreply.github.com>
2022-08-12 20:54:48 +0900

    almostdone

--- order.cpp
+++ order.cpp
@@ -56,6 +56,30 @@ int getCostOfMains(string mains) {
     return 0;
 }
 
+int getCostOfVeg(string veg) {
+    if(veg == "simple") {
+        return 0;
+    }
+    else if(veg == "no_veg") {
+        return -1000;
+    }
+    else {
+        return 500;
+    }
+    
+    return 0;
+}
+
+int getCostOfBev(string bev) {
+    if(bev == "no_bev") {
+        return -1000;
+    }
+    else {
+        return 0;
+    }
+
+    return 0;
+} // size up은 아직 못했다..
 
 string bread;
 string mains;
@@ -88,13 +112,28 @@ int main() {
     if(rm_cheese == "yes") {
         main_cost -= 100;
     }
+    else if(rm_cheese == "no") {
+        main_cost += 0;
+    }
+
+    cout << "Choose vegetable you want. Just say simple if you want only main veg, Cabbage Onion and Tomato. If you don't want any of these, say no_veg" << endl;
+    cin >> veg;
+    veg_basic_cost += getCostOfVeg(veg);
+
+    cout << "Tell any spread you like. Just tell any spread. It's Free!!" << endl;
+    cin >> spread;
+
+    cout << "Lastly, choose your beverage. It costs 1000, +500 if you want to size up. Say no_bev if you don't want it." << endl;
+    cin >> bev;
+    bev_cost += getCostOfBev(bev);
+
 
     int total_cost = bread_cost + main_cost + veg_basic_cost + bev_cost;
     cout << "Your sandwich would cost " << total_cost << "." << endl;
     cin >> money_you_pay;
     cout << "You just paid " << money_you_pay << "!"<< endl;
     cout << "Your change would be " << money_you_pay - total_cost << "." << endl;
-    cout << "Here's your " << bread << " " << mains << " sandwich!" << endl;
+    cout << "Here's your " << bread << " " << mains << " sandwich" << " with " << bev << "!!" << endl;
     cout << "Enjoy your Sandwich!";
     
     return 0;
